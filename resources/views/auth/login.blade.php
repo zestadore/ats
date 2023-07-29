70b4bead4d2f77ae270c425770dcdd9a2474d78a
zestadore <contactzestadore@gmail.com>
2023-07-29 20:25:05 +0530

    Login screen

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,127 @@
+@extends('layouts.app')
+@section('content')
+    <div class="section-authentication-cover">
+        <div class="">
+            <div class="row g-0">
+
+                <div class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex">
+
+                    <div class="card shadow-none bg-transparent shadow-none rounded-0 mb-0">
+                        <div class="card-body">
+                            <img src="{{asset('assets/images/login-images/login-cover.svg')}}" class="img-fluid auth-img-cover-login" width="650" alt=""/>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center">
+                    <div class="card rounded-0 m-3 shadow-none bg-transparent mb-0">
+                        <div class="card-body p-sm-5">
+                            <div class="">
+                                <div class="mb-3 text-center">
+                                    <img src="{{asset('assets/images/logo.jpg')}}" width="60" alt="">
+                                </div>
+                                <div class="text-center mb-4">
+                                    {{-- <h5 class="">Rocker Admin</h5> --}}
+                                    <p class="mb-0">Please log in to your account</p>
+                                </div>
+                                <div class="form-body">
+                                    <form class="row g-3" action="{{ route('login') }}" id="jQueryValidationForm" method="post">@csrf
+                                        <div class="col-12">
+                                            <x-forms.input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" title="Username [ Email ]" name="email" id="email" type="email" required="True"/>
+                                        </div>
+                                        <div class="col-12">
+                                            <x-forms.input class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" title="Password" name="password" id="password" type="password" required="True"/>
+                                        </div>
+                                        <div class="col-md-6"></div>
+                                        <div class="col-md-6 text-end">	<a href="authentication-forgot-password.html">Forgot Password ?</a>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="d-grid">
+                                                <button type="submit" class="btn btn-primary">Sign in</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!--end row-->
+        </div>
+    </div>
+@endsection
+@section('javascripts')
+    <script>
+        $(document).ready(function () {
+            $("#show_hide_password a").on('click', function (event) {
+                event.preventDefault();
+                if ($('#show_hide_password input').attr("type") == "text") {
+                    $('#show_hide_password input').attr('type', 'password');
+                    $('#show_hide_password i').addClass("bx-hide");
+                    $('#show_hide_password i').removeClass("bx-show");
+                } else if ($('#show_hide_password input').attr("type") == "password") {
+                    $('#show_hide_password input').attr('type', 'text');
+                    $('#show_hide_password i').removeClass("bx-hide");
+                    $('#show_hide_password i').addClass("bx-show");
+                }
+            });
+        });
+    </script>
+    <script src="{{asset('assets/plugins/validation/jquery.validate.min.js')}}"></script>
+	{{-- <script src="{{asset('assets/plugins/validation/validation-script.js')}}"></script> --}}
+    <script>
+        $( document ).ready( function () {
+            $( "#jQueryValidationForm" ).validate( {
+                rules: {
+                    yourname: "required",
+                    phone: "required",
+                    username: {
+                        required: true,
+                        minlength: 2
+                    },
+                    password: {
+                        required: true,
+                        minlength: 5
+                    },
+                    confirm_password: {
+                        required: true,
+                        minlength: 5,
+                        equalTo: "#input38"
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    country: "required",
+                    address: "required",
+                    agree: "required"
+                },
+                messages: {
+                    yourname: "Please enter your your name",
+                    phone: "Please enter your phone number",
+                    username: {
+                        required: "Please enter a username",
+                        minlength: "Your username must consist of at least 2 characters"
+                    },
+                    password: {
+                        required: "Please provide a password",
+                        minlength: "Your password must be at least 5 characters long"
+                    },
+                    confirm_password: {
+                        required: "Please provide a password",
+                        minlength: "Your password must be at least 5 characters long",
+                        equalTo: "Please enter the same password as above"
+                    },
+                    email: "Please enter a valid email address",
+                    country: "Please select country",
+                    address: "Please type your message",
+                    agree: "Please accept our policy"
+                },
+            } );
+        } );
+    </script>
+    
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+	<link rel="icon" href="{{asset('assets/images/favicon-32x32.png')}}" type="image/png" />
+	<!--plugins-->
+	<link href="{{asset('assets/plugins/simplebar/css/simplebar.css')}}" rel="stylesheet" />
+	<link href="{{asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
+	<link href="{{asset('assets/plugins/metismenu/css/metisMenu.min.css')}}" rel="stylesheet" />
+	<!-- loader-->
+	<link href="{{asset('assets/css/pace.min.css')}}" rel="stylesheet" />
+	<script src="{{asset('assets/js/pace.min.js')}}"></script>
+	<!-- Bootstrap CSS -->
+	<link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/css/bootstrap-extended.css')}}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<link href="{{asset('assets/css/app.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/css/icons.css')}}" rel="stylesheet">
+    @yield('styles')
+	<title>ATS</title>
+</head>
+
+<body class="">
+	<!--wrapper-->
+	<div class="wrapper">
+        @yield('menus')
+        @yield('content')
+    </div>
+	<!--end wrapper-->
+	<!-- Bootstrap JS -->
+	<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+	<!--plugins-->
+	<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/simplebar/js/simplebar.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/metismenu/js/metisMenu.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js')}}"></script>
+	<!--app JS-->
+	<script src="{{asset('assets/js/app.js')}}"></script>
+    @yield('javascripts')
+</body>
+
+</html>
