38c2fa39519302963c9ac931e42bbe0b9a5893df
zestadore <contactzestadore@gmail.com>
2023-08-01 21:23:51 +0530

    Helper functions

---
+++ app/Helpers/dashboard_helper.php
@@ -0,0 +1,41 @@
+<?php
+  
+use App\Models\Client;
+use App\Models\Candidate;
+use App\Models\JobOpportunity;
+use App\Models\Submission;
+use Carbon\Carbon;
+
+    function getClientsCount(){
+        return Client::count();
+    }
+
+    function getCandidatesCount(){
+        return Candidate::count();
+    }
+
+    function getJobOpportunitiesCount(){
+        return JobOpportunity::count();
+    }
+
+    function getSubmissionsCount(){
+        return Submission::count();
+    }
+
+    function getLastWeekClientsCount(){
+        return Client::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])->count();
+    }
+
+    function getLastWeekCandidatesCount(){
+        return Candidate::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])->count();
+    }
+
+    function getLastWeekJobOpportunitiesCount(){
+        return JobOpportunity::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])->count();
+    }
+
+    function getLastWeekSubmissionsCount(){
+        return Submission::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])->count();
+    }
+
+?>
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,4 +1,95 @@
 @extends('layouts.app')
+@section('title')
+    ATS - Dashboard
+@endsection
 @section('contents')
-    
+    <div class="page-wrapper">
+        <div class="page-content">
+            <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+                <div class="breadcrumb-title pe-3">Dashboard</div>
+                <div class="ps-3">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb mb-0 p-0">
+                            <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}"><i class="bx bx-home-alt"></i></a>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            @if (session('error'))
+                <div class="alert alert-danger border-0 bg-danger alert-dismissible fade show">
+                    <div class="text-white">{{ session('error') }}</div>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            @if (session('success'))
+                <div class="alert alert-primary border-0 bg-primary alert-dismissible fade show">
+                    <div class="text-white">{{ session('success') }}</div>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="card radius-10 border-start border-0 border-4 border-info">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Clients</p>
+                                    <h4 class="my-1 text-info">{{getClientsCount()}}</h4>
+                                    <p class="mb-0 font-13">+{{getLastWeekClientsCount()}} from last week</p>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-blues text-white ms-auto"><i class="bx bxs-cart"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="card radius-10 border-start border-0 border-4 border-warning">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Candidates</p>
+                                    <h4 class="my-1 text-info">{{getCandidatesCount()}}</h4>
+                                    <p class="mb-0 font-13">+{{getLastWeekCandidatesCount()}} from last week</p>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-blues text-white ms-auto"><i class="bx bxs-cart"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="card radius-10 border-start border-0 border-4 border-danger">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Job Opportunities</p>
+                                    <h4 class="my-1 text-info">{{getJobOpportunitiesCount()}}</h4>
+                                    <p class="mb-0 font-13">+{{getLastWeekJobOpportunitiesCount()}} from last week</p>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-blues text-white ms-auto"><i class="bx bxs-cart"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="card radius-10 border-start border-0 border-4 border-success">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Job Submissions</p>
+                                    <h4 class="my-1 text-info">{{getSubmissionsCount()}}</h4>
+                                    <p class="mb-0 font-13">+{{getLastWeekSubmissionsCount()}} from last week</p>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-blues text-white ms-auto"><i class="bx bxs-cart"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
