da4b1cba1e7befd525201872bb47c28c7c270440
zestadore <contactzestadore@gmail.com>
2023-08-29 22:21:27 +0530

    Login page works

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,56 +3,40 @@
     Login
 @endsection
 @section('content')
-    <div class="section-authentication-cover">
-        <div class="">
-            <div class="row g-0">
-
-                <div class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex" style="background:#d9eefd;">
-
-                    <div class="card shadow-none bg-transparent shadow-none rounded-0 mb-0">
-                        <div class="card-body">
-                            <img src="{{asset('assets/images/login-images/ats_login.png')}}" class="img-fluid auth-img-cover-login" width="100%" alt=""/>
+    <div class="page-holder align-items-center py-4 bg-gray-100 vh-100">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 px-lg-4">
+                    <div class="card">
+                        <div class="card-header px-lg-5">
+                            <div class="card-heading text-primary">Bubbly Dashboard</div>
                         </div>
-                    </div>
-                    
-                </div>
-
-                <div class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center">
-                    <div class="card rounded-0 m-3 shadow-none bg-transparent mb-0">
-                        <div class="card-body p-sm-5">
-                            <div class="">
-                                <div class="mb-3 text-center">
-                                    <img src="{{asset('uploads/site_logo/'.env('SITE_LOGO',''))}}" width="60" alt="">
+                        <div class="card-body p-lg-5">
+                            <h3 class="mb-4">Hi, welcome back! 👋👋</h3>
+                            <p class="text-muted text-sm mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                            <form id="loginForm" action="index.html">
+                                <div class="form-floating mb-3">
+                                <input class="form-control" id="floatingInput" type="email" placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
                                 </div>
-                                <div class="text-center mb-4">
-                                    {{-- <h5 class="">Rocker Admin</h5> --}}
-                                    <p class="mb-0">Welcome to Amnext's Application Tracking System (ATS). Please enter your login credentials below to access your account.</p>
+                                <div class="form-floating mb-3">
+                                <input class="form-control" id="floatingPassword" type="password" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
                                 </div>
-                                <div class="form-body">
-                                    <form class="row g-3" action="{{ route('login') }}" id="jQueryValidationForm" method="post">@csrf
-                                        <div class="col-12">
-                                            <x-forms.input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" title="Username [Email Addrerss] : " name="email" id="email" type="email" required="True"/>
-                                        </div>
-                                        <div class="col-12">
-                                            <x-forms.input class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" title="Password : " name="password" id="password" type="password" required="True"/>
-                                        </div>
-                                        <div class="col-md-6"><p> </p></div>
-                                        {{-- <div class="col-md-6 text-end">	<a href="authentication-forgot-password.html">Forgot Password ?</a> --}}
-                                        </div>
-                                        <div class="col-12">
-                                            <div class="d-grid">
-                                                <button type="submit" class="btn btn-primary">Sign in</button>
-                                            </div>
-                                        </div>
-                                    </form>
+                                <div class="form-check mb-3">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember">
+                                <label class="form-check-label" for="remember">Remember me</label>
                                 </div>
-                            </div>
+                                <button class="btn btn-primary btn-lg" type="submit">Submit</button>
+                            </form>
+                        </div>
+                        <div class="card-footer px-lg-5 py-lg-4">
                         </div>
                     </div>
                 </div>
-
+                <div class="col-lg-6 col-xl-5 ms-xl-auto px-lg-4 text-center text-primary"><img class="img-fluid mb-4" width="300" src="img/drawkit-illustration.svg" alt="" style="transform: rotate(10deg)">
+                </div>
             </div>
-            <!--end row-->
         </div>
     </div>
 @endsection
